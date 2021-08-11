b3d87e7723aaa48a75779eccd104050f5e64ca3c
Saul Suaste <sumsaul94@hotmail.com>
2021-08-11 17:47:17 -0500

    Se temino el Header, css y js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Se obtiene el tamaño del body menos el tamaño de la ventana
+const altura = document.body.scrollHeight - window.innerHeight;
+const fondo = document.getElementById("fondo");
+
+window.onscroll = () => { //al hacer scrool ejecuta
+    //el número de píxeles que el documento se está desplazando actualmente a lo largo del eje vertical / la altura, por 400px
+    const anchoFondo = (window.pageYOffset / altura) * 350;
+    if(anchoFondo <= 100){
+        fondo.style.width = anchoFondo + "%"; //agrega la cantidad como porcentaje de width a la clase fondo
+    }
+}
+
