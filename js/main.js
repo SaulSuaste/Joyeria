c4374ca93d787d96a2b6c0f952c177e077ba38cc
Saul Suaste <sumsaul94@hotmail.com>
2021-08-11 21:17:49 -0500

    Pagina terminada

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const fondo = document.getElementById("fondo");
 
 window.onscroll = () => { //al hacer scrool ejecuta
     //el número de píxeles que el documento se está desplazando actualmente a lo largo del eje vertical / la altura, por 400px
-    const anchoFondo = (window.pageYOffset / altura) * 350;
+    const anchoFondo = (window.pageYOffset / altura) * 450;
     if(anchoFondo <= 100){
         fondo.style.width = anchoFondo + "%"; //agrega la cantidad como porcentaje de width a la clase fondo
     }
